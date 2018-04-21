dc662697edc33c331d0e89bf79fb9b0ddc6c6f46
Austin Burk <apburk@sudomemo.net>
2018-04-21 17:26:59 -0400

    Now we check for xxd and extract the nbt files to a folder based on the input file's name.

--- region-parser/region-parser.sh
+++ region-parser/region-parser.sh
@@ -39,8 +39,16 @@ if [ ! -f $1 ]; then
 	usage;
 fi
 
-echo "[INFO] Deleting all NBT files in ./out"
-rm -f out/*.nbt
+if ! command -v xxd > /dev/null; then echo '[ERROR] Could not find xxd, please install first.'; exit 1; fi
+
+filename=$(echo -n $1|sed 's/\.nbt//g')
+
+if [ ! -d ${filename}_nbt ]; then
+	mkdir ${filename}_nbt;
+else
+	echo "[INFO] Deleting all NBT files in ./${filename}_nbt"
+	rm -f ${filename}_nbt/*.nbt
+fi
 
 chunk_number=-1 # until we get the location of the chunk I guess
 
@@ -75,11 +83,11 @@ for location in $(head -c4096 $1 |xxd -p -c 4); do
 	if [[ $chunk_length -gt 0 ]]; then	#skip zero-length chunks
 		#log_debug "[DEBUG] chunk timestamp raw (hex) = $(tail -c+$(($offset+4096)) $1|head -c4|xxd -p -c 4)"
 		#log_debug "[DEBUG] chunk timestamp = $timestamp = $(date -d @$timestamp)"
-		echo "[INFO] Extracting chunk $chunk_number (modify time: $(date -d @$timestamp)) to out/$chunk_number.nbt"
+		echo "[INFO] Extracting chunk $chunk_number (modify time: $(date -d @$timestamp)) to ${filename}_nbt/$chunk_number.nbt"
 
 		# We tack on the gzip magic at the beginning, but I believe it's missing something (e.g. crc) at the end of the file. As such, gzip normally errors out, so we ignore that.
 		# still seems to be valid after decompression :)
 
-		gzip -dcq <(printf "\x1f\x8b\x08\x00\x00\x00\x00\x00" |cat - <(tail -c+$((($offset*4096)+6)) $1|head -c$(($chunk_length -1 )))) 2>/dev/null > out/$chunk_number.nbt
+		gzip -dcq <(printf "\x1f\x8b\x08\x00\x00\x00\x00\x00" |cat - <(tail -c+$((($offset*4096)+6)) $1|head -c$(($chunk_length -1 )))) 2>/dev/null > ${filename}_nbt/$chunk_number.nbt
 	fi;
 done
