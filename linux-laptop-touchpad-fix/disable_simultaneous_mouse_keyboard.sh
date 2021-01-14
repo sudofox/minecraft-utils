434017e6efc6b57708113e55a94abfef6e2202ab
Austin Burk <apburk@gmail.com>
2021-01-14 11:17:11 -0500

    Add files via upload

---
+++ linux-laptop-touchpad-fix/disable_simultaneous_mouse_keyboard.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+XINPUT_DEV=$(xinput |grep -Po "SynPS/2 Synaptics TouchPad.*id=\K.+?(?=\t)")
+
+OPTION_ID=$(xinput --list-props $XINPUT_DEV|grep -Po "libinput Disable\ While\ Typing\ Enabled\ \(\K.+?(?=\))")
+
+xinput --set-prop $XINPUT_DEV $OPTION_ID 1
+
+echo "Updated Option ID $OPTION_ID of device $XINPUT_DEV to 1 (true). You can no longer move the mouse and type at the same time"
