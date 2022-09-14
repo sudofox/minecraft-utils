84f869af2a208c19a6294399928d1cfc336c1e46
Austin Burk <apburk@gmail.com>
2022-09-14 12:03:51 -0400

    Update region-parser.sh

--- region-parser/region-parser.sh
+++ region-parser/region-parser.sh
@@ -40,12 +40,12 @@ usage() {
 if [ $# -eq 0 ]; then usage; fi
 
 # File exists?
-
 if [ ! -f $1 ]; then
 	echo "[ERROR] Couldn't open chunk file $1"
 	usage
 fi
 
+# xxd installed?
 if ! command -v xxd >/dev/null; then
 	echo '[ERROR] Could not find xxd, please install first.'
 	exit 1
@@ -60,7 +60,7 @@ else
 	rm -f ${filename}_nbt/*.nbt
 fi
 
-chunk_number=-1 # until we get the location of the chunk I guess
+chunk_number=-1
 
 for location in $(head -c4096 $1 | xxd -p -c 4); do
 
@@ -77,7 +77,7 @@ for location in $(head -c4096 $1 | xxd -p -c 4); do
 
 	timestamp=$((0x$(tail -c+$((($offset * 4) + 4097)) $1 | head -c4 | xxd -p -c 4)))
 
-	#obtain the length and compression type
+	# obtain the length and compression type
 
 	chunkheader=$(tail -c+$(($offset * 4096 + 1)) $1 | head -c5 | xxd -p -c 5)
 	log_debug "[DEBUG] chunk $chunk_number header: "$(echo $chunkheader | xxd -r -p | xxd -c 8)
@@ -86,15 +86,14 @@ for location in $(head -c4096 $1 | xxd -p -c 4); do
 	chunk_length=$(((16777216 * 0x${chunkheader:0:2}) + (65536 * 0x${chunkheader:2:2}) + (256 * 0x${chunkheader:4:2}) + 0x${chunkheader:6:2}))
 	log_debug "[DEBUG] Chunk data (start position, chunk_length): "$(($offset * 4096))", $chunk_length"
 
-	# Decode chunk header.
-	if [[ $chunk_length -gt 0 ]]; then #skip zero-length chunks
-		#log_debug "[DEBUG] chunk timestamp raw (hex) = $(tail -c+$(($offset+4096)) $1|head -c4|xxd -p -c 4)"
-		#log_debug "[DEBUG] chunk timestamp = $timestamp = $(date -d @$timestamp)"
+	# Decode chunk header
+	if [[ $chunk_length -gt 0 ]]; then # skip zero-length chunks
+		# log_debug "[DEBUG] chunk timestamp raw (hex) = $(tail -c+$(($offset+4096)) $1|head -c4|xxd -p -c 4)"
+		# log_debug "[DEBUG] chunk timestamp = $timestamp = $(date -d @$timestamp)"
 		echo "[INFO] Extracting chunk $chunk_number (modify time: $(date -d @$timestamp)) to ${filename}_nbt/$chunk_number.nbt"
 
 		# We tack on the gzip magic at the beginning, but I believe it's missing something (e.g. crc) at the end of the file. As such, gzip normally errors out, so we ignore that.
 		# still seems to be valid after decompression :)
-
 		gzip -dcq <(printf "\x1f\x8b\x08\x00\x00\x00\x00\x00" | cat - <(tail -c+$((($offset * 4096) + 6)) $1 | head -c$(($chunk_length - 1)))) 2>/dev/null >${filename}_nbt/$chunk_number.nbt
 	fi
 done
